b5d54d7d759a64c36f24cdba8b364c7ff300cf83
alapshin <alapshin@plesk.com>
2021-12-27 10:01:23 +0700

    task 1

---
+++ task1.binSum.php
@@ -0,0 +1,9 @@
+<?php
+function binarySum(string $binFirst, string $binSecond){
+    $decFirst = bindec($binFirst);
+    $decSecond = bindec($binSecond);
+    $decRes = $decSecond + $decFirst;
+    return decbin($decRes);
+}
+
+echo binarySum ('1101','1101');
