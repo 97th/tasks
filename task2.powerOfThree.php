63218419f2a8af314b88dca08d0fc4e2ae6fc151
alapshin <alapshin@plesk.com>
2021-12-28 10:16:18 +0700

    task 2

--- task2.powerOfThree.php
+++ task2.powerOfThree.php
@@ -1,19 +1,16 @@
 <?php
 function isPowerOfThree(int $number)
 {
-    if ($number == 1)
-    {
-        return print_r('true 3^0'.PHP_EOL);
+    if ($number == 1){
+        return true;
     }
-    for ($i=1;$i<100;$i++)
-    {
-        $result = pow($number, (1/$i));
-        if ($result == 3)
-        {
-            return print_r('true 3^'.$i.PHP_EOL);
+    for ($i = 1; $i < 100; $i++){
+        $result = pow($number, (1 / $i));
+        if ($result == 3){
+            return true;
         }
     }
-    return print_r('false');
+    return false;
 }
 
-isPowerOfThree(31381059609);
+echo isPowerOfThree(31381059609);
