a6d5f4411476832155d3f3555f8102d42fd70928
alapshin <alapshin@plesk.com>
2021-12-28 11:03:21 +0700

    task 7

--- task7.happyTicket.php
+++ task7.happyTicket.php
@@ -1,20 +1,25 @@
 <?php
-function isHappy($number){
+function isHappy($number)
+{
     $line = strval($number);
     $numberOfDigits = strlen($line);
-    if ($numberOfDigits % 2 != 0){
+    if ($numberOfDigits % 2 != 0) {
         return print_r('Uneven');
     }
     $firstPart = $numberOfDigits / 2;
     $firstRes = 0;
     $secondRes = 0;
-    for ($i = 0; $i < $firstPart ;$i++){
+    for ($i = 0; $i < $firstPart ;$i++) {
         $firstRes += $line[$i];
     }
-    for ($i = $firstPart; $i < $numberOfDigits ;$i++){
+    for ($i = $firstPart; $i < $numberOfDigits; $i++) {
         $secondRes += $line[$i];
     }
-    $firstRes == $secondRes ? print_r('true') : print_r('false');
-
+    if ($firstRes == $secondRes) {
+        return true;
+    }
+    else
+        return false;
 }
-echo (isHappy(385916));
+
+echo isHappy(212005);
