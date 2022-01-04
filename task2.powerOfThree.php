efefabaa5a25b44b1a67d338fb4ace7be7817228
alapshin <alapshin@plesk.com>
2022-01-04 20:56:52 +0700

    task 2

--- task2.powerOfThree.php
+++ task2.powerOfThree.php
@@ -1,12 +1,12 @@
 <?php
 function isPowerOfThree(int $number)
 {
-    if ($number == 1){
+    if ($number == 1) {
         return true;
     }
-    for ($i = 1; $i < 100; $i++){
+    for ($i = 1; $i < 100; $i++) {
         $result = pow($number, (1 / $i));
-        if ($result == 3){
+        if ($result === 3) {
             return true;
         }
     }
