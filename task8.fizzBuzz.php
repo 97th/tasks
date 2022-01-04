ad2811660c78ffe49e260fcbdabed849d377ffb4
alapshin <alapshin@plesk.com>
2022-01-04 21:42:35 +0700

    task 8

--- task8.fizzBuzz.php
+++ task8.fizzBuzz.php
@@ -1,19 +1,17 @@
 <?php
 function fizzBuzz($begin, $end)
 {
-    if ($begin <= $end) {
-        for ($i = $begin; $i <= $end; $i++) {
-            if (($i % 3 == 0) && ($i % 5 == 0)) {
-                print_r('FizzBuzz ');
-            } elseif ($i % 3 == 0) {
-                print_r('Fizz ');
-            } elseif ($i % 5 == 0) {
-                print_r('Buzz ');
-            } else {
-                print_r($i . ' ');
-            }
+    for ($i = $begin; $i <= $end; $i++) {
+        if (($i % 3 === 0) && ($i % 5 === 0)) {
+            print_r('FizzBuzz ');
+        } elseif ($i % 3 === 0) {
+            print_r('Fizz ');
+        } elseif ($i % 5 === 0) {
+            print_r('Buzz ');
+        } else {
+            print_r($i . ' ');
         }
-    }
+       }
 }
 
 echo fizzBuzz(44,120);
