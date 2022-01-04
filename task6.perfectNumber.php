98e9c787e333509f4c5b10da9a351b398c1eb11e
alapshin <alapshin@plesk.com>
2022-01-04 21:29:37 +0700

    task 6

--- task6.perfectNumber.php
+++ task6.perfectNumber.php
@@ -3,14 +3,11 @@ function isPerfect($number)
 {
     $sumOfDiv = 0;
     for ($i = 1; $i < $number; $i++) {
-        if ($number % $i == 0) {
+        if ($number % $i === 0) {
             $sumOfDiv += $i;
         }
     }
-    if ($sumOfDiv == $number) {
-        return true;
-    }
-    else
-        return false;
+    return $sumOfDiv === $number;
 }
+
 echo isPerfect(8128);
