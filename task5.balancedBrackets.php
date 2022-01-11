78bbdcac2d070b35d20ede41cd08dabdc1021cec
alapshin <alapshin@plesk.com>
2022-01-11 09:14:11 +0700

    task 5

---
+++ task5.balancedBrackets.php
@@ -0,0 +1,24 @@
+<?php
+function isBalanced(string $line)
+{
+    if ($line === '') {
+        return true;
+    }
+    $balance = 0;
+    $lineLength = strlen($line);
+    for ($i = 0; $i < $lineLength; $i++) {
+        if ($line[$i] !== '(' && $line[$i] !== ')') {
+            return false;
+        }
+        if ($line[$i] === '(') {
+            $balance++;
+        } else {
+            $balance--;
+        }
+        if ($balance < 0) {
+            return false;
+        }
+    }
+    return $balance === 0;
+}
+echo isBalanced('()()');
