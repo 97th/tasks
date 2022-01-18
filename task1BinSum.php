74aea462cdf22a900974ef925fa94464f2485050
alapshin <alapshin@plesk.com>
2022-01-18 23:26:55 +0700

    redo as classes

---
+++ task1BinSum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MyApp\tasks;
+
+class task1BinSum
+{
+    public function binarySum(string $binFirst, string $binSecond): string
+    {
+        $decFirst = bindec($binFirst);
+        $decSecond = bindec($binSecond);
+        $decRes = $decSecond + $decFirst;
+        return decbin($decRes);
+    }
+
+    //echo binarySum ('1101', '1101');
+}
---
+++ task2PowerOfThree.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MyApp\tasks;
+
+class task2PowerOfThree
+{
+    public function isPowerOfThree(int $number)
+    {
+        if ($number === 1) {
+            return true;
+        }
+        for ($i = 1; $i < 100; $i++) {
+            $result = pow($number, (1 / $i));
+            if ((float)$result === 3.0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+//echo isPowerOfThree(31381059609);
