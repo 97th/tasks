943e0f7549a8966b5fa3a1f237b30f44393e18c2
alapshin <alapshin@plesk.com>
2022-01-20 16:05:47 +0700

    rewrote task 8 for tests

--- src/tasks/Task8FizzBuzz.php
+++ src/tasks/Task8FizzBuzz.php
@@ -3,19 +3,28 @@ namespace MyApp\tasks;
 
 class Task8FizzBuzz
 {
+    public function generateFizzBuzz ($beginFizz, $endFizz): string
+    {
+        return $this->fizzBuzz($beginFizz, $endFizz);
+    }
     public function fizzBuzz($begin, $end)
     {
+        $output = "";
         for ($i = $begin; $i <= $end; $i++) {
             if (($i % 3 === 0) && ($i % 5 === 0)) {
-                print_r('FizzBuzz ');
+                $output .= 'FizzBuzz ';
+                //print_r('FizzBuzz ');
             } elseif ($i % 3 === 0) {
-                print_r('Fizz ');
+                $output .= 'Fizz ';
+                //print_r('Fizz ');
             } elseif ($i % 5 === 0) {
-                print_r('Buzz ');
+                //print_r('Buzz ');
+                $output .= 'Buzz ';
             } else {
-                print_r($i . ' ');
+                $output .= strval($i) . ' ';
+                //print_r($i . ' ');
             }
         }
+        return $output;
     }
 }
-//echo fizzBuzz(44,120);
